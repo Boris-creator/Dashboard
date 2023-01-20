e4b3a4b2ce21194db7c46fc3382d133f62f9885c
Boris <BK.youngmath@icloud.com>
2023-01-20 18:45:57 +0300

    Исправлен баг с маской

--- src/directives/mask.ts
+++ src/directives/mask.ts
@@ -4,14 +4,26 @@ type Input = HTMLInputElement & { cleave: any };
 function formatValue(e: InputEvent) {
   const el = e.target as Input;
   const formatter = new AsYouType(el.cleave);
-  const start = el.selectionStart,
-    end = el.selectionEnd;
+  const start = el.selectionStart ?? 0;
+  const currentSymbol = el.value.slice(0, start).replace(/\D/g, "").length;
   const event = new Event("input", { bubbles: true });
   requestAnimationFrame(function () {
     el.value = formatter.input(el.value);
-
+    const newStart = [...el.value].reduce(
+      (acc, cur) => {
+        if (acc.d == currentSymbol) {
+          return acc;
+        }
+        acc.i++;
+        if (/\d/.test(cur)) {
+          acc.d++;
+        }
+        return acc;
+      },
+      { d: 0, i: 0 }
+    ).i;
     el.dispatchEvent(event);
-    el.setSelectionRange(start, end);
+    el.setSelectionRange(newStart, newStart);
   });
 }
 export const cleave: ObjectDirective = {
